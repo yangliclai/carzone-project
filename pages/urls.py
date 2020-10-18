5f12b40469cf0328a349055c27a08efbeb150496
yangliclai <yangliclai@gmail.com>
2020-10-18 02:40:16 -0400

    added cars app and fetched the data

--- pages/urls.py
+++ pages/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    # path('cars', views.cars, name='cars'),
     path('about', views.about, name='about'),
     path('services', views.services, name='services'),
     path('contact', views.contact, name='contact'),
